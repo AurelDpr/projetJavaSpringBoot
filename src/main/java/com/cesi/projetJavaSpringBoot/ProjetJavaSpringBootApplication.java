f72c2bdb3479229be36a88986eb068ebaa8ce4b9
Aurelien <aurel.dpr@gmail.com>
2019-11-20 21:09:49 +0100

    create java spring boot project

---
+++ src/main/java/com/cesi/projetJavaSpringBoot/ProjetJavaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.cesi.projetJavaSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetJavaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetJavaSpringBootApplication.class, args);
+	}
+
+}
